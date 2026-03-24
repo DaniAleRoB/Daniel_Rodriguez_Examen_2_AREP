404e3c67dd1bb3fb7e434a198f46bb7b00bd4f5a
DaniAleRoB <daniel.rodriguez-b@mail.escuelaing.edu.co>
2026-03-24 17:18:57 -0500

    Añadiendo el modelo

---
+++ src/main/java/co/escuelaing/edu/co/examen2/model/Math.java
@@ -0,0 +1,17 @@
+package co.escuelaing.edu.co.examen2.model;
+
+public class Math {
+    private String operation;
+    private int input;
+    private String output;
+
+    public Math(String operation, int input, String output) {
+        this.operation = operation;
+        this.input = input;
+        this.output = output;
+    }
+
+    public String getOperation() { return operation; }
+    public int getInput() { return input; }
+    public String getOutput() { return output; }
+}
