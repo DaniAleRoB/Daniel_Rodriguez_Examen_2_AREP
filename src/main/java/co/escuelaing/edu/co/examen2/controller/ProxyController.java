22564f06ff1d638ac85b8d4e262ff70681d1ca37
DaniAleRoB <daniel.rodriguez-b@mail.escuelaing.edu.co>
2026-03-24 17:59:54 -0500

    Añadiendo proxy

--- src/main/java/co/escuelaing/edu/co/examen2/controller/ProxyController.java
+++ src/main/java/co/escuelaing/edu/co/examen2/controller/ProxyController.java
@@ -1,5 +1,33 @@
 package co.escuelaing.edu.co.examen2.controller;
 
+import co.escuelaing.edu.co.examen2.service.HttpService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ProxyController {
-    
+
+    private final HttpService httpService = new HttpService();
+
+    private String service1 = System.getenv("SERVICE1_URL");
+    private String service2 = System.getenv("SERVICE2_URL");
+
+    @GetMapping("/factors")
+    public String factors(@RequestParam String value) {
+
+        try {
+            return httpService.get(service1 + "/math/factors?value=" + value);
+        } catch (Exception e) {
+            return httpService.get(service2 + "/math/factors?value=" + value);
+        }
+    }
+
+    @GetMapping("/primes")
+    public String primes(@RequestParam String value) {
+
+        try {
+            return httpService.get(service1 + "/math/primes?value=" + value);
+        } catch (Exception e) {
+            return httpService.get(service2 + "/math/primes?value=" + value);
+        }
+    }
 }
